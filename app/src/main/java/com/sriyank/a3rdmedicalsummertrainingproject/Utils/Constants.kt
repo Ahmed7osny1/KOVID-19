0ca7a3fe19faf371d10449a790550671fb7f7521
Ahmed <Ahmedhosny6688@gmail.com>
2022-08-14 00:15:09 +0200

    initiation project

---
+++ app/src/main/java/com/sriyank/a3rdmedicalsummertrainingproject/Utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.sriyank.a3rdmedicalsummertrainingproject.Utils
+
+object Constants {
+
+    const val SHARED_PREFS_FILENAME = "apiauthtest"
+    const val SERVER_NAME = "https://boiling-spire-83238.herokuapp.com"
+
+}
