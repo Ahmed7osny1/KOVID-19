617cb33a9593343ef27ea831b5ad05c101c5c33f
Ahmed <Ahmedhosny6688@gmail.com>
2022-08-16 00:51:06 +0200

    home completed

---
+++ app/src/main/java/com/sriyank/a3rdmedicalsummertrainingproject/service/VaccineReservationActivity.kt
@@ -0,0 +1,12 @@
+package com.sriyank.a3rdmedicalsummertrainingproject.service
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sriyank.a3rdmedicalsummertrainingproject.R
+
+class VaccineReservationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vaccine_reservation)
+    }
+}
