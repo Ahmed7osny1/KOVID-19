55a907e49c42fcdfe4205664956eecc90c4a1081
Ahmed Hosny <ahmedhosny6688@gmail.com>
2022-09-08 22:04:29 +0200

    Changes

--- app/src/main/java/com/sriyank/a3rdmedicalsummertrainingproject/service/VaccineReservationActivity.kt
+++ app/src/main/java/com/sriyank/a3rdmedicalsummertrainingproject/service/VaccineReservationActivity.kt
@@ -1,12 +1,273 @@
 package com.sriyank.a3rdmedicalsummertrainingproject.service
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.toolbox.Volley
+import com.sriyank.a3rdmedicalsummertrainingproject.HomeActivity
 import com.sriyank.a3rdmedicalsummertrainingproject.R
+import com.sriyank.a3rdmedicalsummertrainingproject.Utils.MyRequest
+import com.sriyank.a3rdmedicalsummertrainingproject.Utils.MyRequestArray
+import kotlinx.android.synthetic.main.activity_vaccine_reservation.*
+import org.json.JSONObject
+import java.util.*
+
 
 class VaccineReservationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_vaccine_reservation)
+
+        backBtnVaccie.setOnClickListener { onBackPressed() }
+
+        val items1 = getData()
+
+        val arrayAdapterTest = ArrayAdapter(this, R.layout.dropdown_item, items1)
+
+        menuTests.setAdapter(arrayAdapterTest)
+
+        val TestSelected: String = menuTests.text.toString()
+
+        // choose healthCare center
+        val items2 = getDataCare()
+
+        val arrayAdapterCenter = ArrayAdapter(this, R.layout.dropdown_item, items2)
+
+        menuCenter.setAdapter(arrayAdapterCenter)
+
+        val LocationSelected: String = menuCenter.text.toString()
+
+        time.setOnClickListener {
+
+            val timePicker: TimePickerDialog = TimePickerDialog(
+                this,
+                timePicker(),
+                12,
+                10,
+                false
+            )
+
+            timePicker.show()
+
+        }
+
+        Date.setOnClickListener { calenderShow() }
+
+        confirm.setOnClickListener {
+
+            if (time.text!!.isNotEmpty() && Date.text!!.isNotEmpty()) {
+                ReserveApi(
+                    TestSelected,
+                    Date.text.toString(),
+                    time.text.toString(),
+                    LocationSelected
+                )
+            }
+            else {
+
+
+                if (Date.text!!.isEmpty())
+                    Toast.makeText(this, "Please select Date", Toast.LENGTH_LONG).show()
+
+                if (time.text!!.isEmpty())
+                    Toast.makeText(this, "Please select Time", Toast.LENGTH_LONG).show()
+
+            }
+        }
+
     }
+
+    private fun ReserveApi(type: String,Date: String,time: String,location: String) {
+
+        val params = JSONObject()
+
+        params.put("test_name", type)
+        params.put("test_date", Date)
+        params.put("test_time", time)
+        params.put("test_patient_health_name", location)
+
+
+        Log.d("mytag", "Button clicked")
+
+        // send request
+        val queue = Volley.newRequestQueue(this)
+        val request = MyRequest(
+            this,
+            Request.Method.POST,
+            "/test-reservation",
+            params,
+            { response ->
+
+                Log.d("mytag", "response = $response")
+                Toast.makeText(this@VaccineReservationActivity,
+                    "$response",Toast.LENGTH_LONG).show()
+                // goto Home activity
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+                finish()
+
+                // if there is an error (wrong email or password)
+                if (response.has("error")) {
+                    val errorMesssage = response.getString("error")
+                    Toast.makeText(this@VaccineReservationActivity, errorMesssage,
+                        Toast.LENGTH_SHORT)
+                        .show()
+
+                }
+            },
+            { error ->
+                Log.e("mytag",
+                    "Error: $error - Status Code = ${error.networkResponse?.statusCode}")
+                Toast.makeText(this, "Connection error", Toast.LENGTH_SHORT).show()
+            }
+        )
+        queue.add(request)
+
+    }
+
+
+    private fun calenderShow(){
+
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        val datePickerDialog = DatePickerDialog(this,
+            DatePickerDialog.OnDateSetListener
+            { view, year, monthOfYear, dayOfMonth ->
+                Date.setText("$year-${(monthOfYear + 1)}-$dayOfMonth")
+            }, year, month, day
+        )
+        //currentTimeMillis
+        datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis())
+        datePickerDialog.show()
+
+    }
+
+    private fun timePicker() : TimePickerDialog.OnTimeSetListener {
+
+        // listener which is triggered when the
+        // time is picked from the time picker dialog
+        val timePickerDialogListener: TimePickerDialog.OnTimeSetListener =
+
+            TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
+                // logic to properly handle
+                // the picked timings by user
+                val formattedTime: String = when {
+                    hourOfDay == 0 -> {
+                        if (minute < 10) {
+                            "${hourOfDay + 12}:0${minute} am"
+                        } else {
+                            "${hourOfDay + 12}:${minute} am"
+                        }
+                    }
+                    hourOfDay > 12 -> {
+                        if (minute < 10) {
+                            "${hourOfDay - 12}:0${minute} pm"
+                        } else {
+                            "${hourOfDay - 12}:${minute} pm"
+                        }
+                    }
+                    hourOfDay == 12 -> {
+                        if (minute < 10) {
+                            "${hourOfDay}:0${minute} pm"
+                        } else {
+                            "${hourOfDay}:${minute} pm"
+                        }
+                    }
+                    else -> {
+                        if (minute < 10) {
+                            "${hourOfDay}:${minute} am"
+                        } else {
+                            "${hourOfDay}:${minute} am"
+                        }
+                    }
+                }
+                time.setText(formattedTime)
+            }
+        return timePickerDialogListener
+    }
+
+    private fun getData(): ArrayList<String> {
+
+        val list = arrayListOf<String>()
+
+        Log.d("mytag", "Button clicked")
+
+        // send request
+        val queue = Volley.newRequestQueue(this)
+        val request = MyRequestArray(
+            this,
+            Request.Method.GET,
+            "/available-vaccines",
+            null,
+            { response ->
+
+                Log.d("mytag", "$response")
+
+                for (i in 0 until  response.length()){
+
+                    val test: JSONObject = response.getJSONObject(i)
+
+                    // get the current student (json object) data
+                    list.add(test.getString("vaccine_name"))
+
+                }
+                Log.d("mytag", "$list")
+
+            },
+            { error ->
+                Log.e("mytag", "Error: $error - Status Code = ${error.networkResponse?.statusCode}")
+                Toast.makeText(this, "Connection error", Toast.LENGTH_SHORT).show()
+            }
+        )
+        queue.add(request)
+
+        return list
+    }
+
+    private fun getDataCare(): ArrayList<String> {
+
+        val arrayList = arrayListOf<String>()
+
+        Log.d("mytag", "Button clicked")
+
+        // send request
+        val queue = Volley.newRequestQueue(this)
+        val request = MyRequestArray(
+            this,
+            Request.Method.GET,
+            "/avaiable-healthcare-centers",
+            null,
+            { response ->
+
+                Log.d("mytag", "$response")
+
+                for (i in 0 until  response.length()){
+
+                    val student: JSONObject = response.getJSONObject(i)
+
+                    // get the current student (json object) data
+                    arrayList.add(student.getString("hc_name"))
+
+                }
+                Log.d("mytag", "$arrayList")
+
+            },
+            { error ->
+                Log.e("mytag", "Error: $error - Status Code = ${error.networkResponse?.statusCode}")
+                Toast.makeText(this, "Connection error", Toast.LENGTH_SHORT).show()
+            }
+        )
+        queue.add(request)
+        return arrayList
+    }
+
 }
--- app/src/main/java/com/sriyank/a3rdmedicalsummertrainingproject/service/chattingDoctorActivity.kt
+++ app/src/main/java/com/sriyank/a3rdmedicalsummertrainingproject/service/chattingDoctorActivity.kt
@@ -1,12 +1,158 @@
 package com.sriyank.a3rdmedicalsummertrainingproject.service
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.toolbox.Volley
+import com.sriyank.a3rdmedicalsummertrainingproject.HomeActivity
 import com.sriyank.a3rdmedicalsummertrainingproject.R
+import com.sriyank.a3rdmedicalsummertrainingproject.Utils.MyRequest
+import com.sriyank.a3rdmedicalsummertrainingproject.Utils.MyRequestArray
+import kotlinx.android.synthetic.main.activity_chatting_doctor.*
+import org.json.JSONObject
+
 
 class chattingDoctorActivity : AppCompatActivity() {
+
+    private lateinit var doctorSelected: String
+    private lateinit var EmailSend: String
+    private var EmailSeslected = arrayListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chatting_doctor)
+
+        backBtn.setOnClickListener { onBackPressed() }
+
+        val items1 = getData()
+
+        val arrayAdapterDoctor = ArrayAdapter(this, R.layout.dropdown_item, items1)
+
+        menuDoctor.setAdapter(arrayAdapterDoctor)
+
+        menuDoctor.onItemClickListener =
+            OnItemClickListener { adapterView, view, position, id ->
+                doctorSelected = arrayAdapterDoctor.getItem(position).toString()
+                EmailSend = EmailSeslected[position]
+                //Toast.makeText(this,"$doctorSelected : $EmailSend",Toast.LENGTH_LONG).show()
+            }
+
+        sendDoctor.setOnClickListener {
+
+            if(mEditMassage.text!!.isNotEmpty() && messageContent.text!!.isNotEmpty()
+                && doctorSelected.isNotEmpty()) {
+
+                sendMessage(EmailSend, messageContent.text.toString())
+
+                Toast.makeText(this, "SEND SUCCESSFUL", Toast.LENGTH_LONG).show()
+            }else{
+
+                if(doctorSelected.isEmpty())
+                    Toast.makeText(this, "Choose your Doctor", Toast.LENGTH_LONG).show()
+
+                if(mEditMassage.text!!.isEmpty())
+                    Toast.makeText(this, "Enter your Title Message", Toast.LENGTH_LONG).show()
+
+                if(messageContent.text!!.isEmpty())
+                    Toast.makeText(this, "Enter your Content Message", Toast.LENGTH_LONG).show()
+
+            }
+        }
+
+
     }
+
+    private fun getData(): ArrayList<String> {
+
+        val arrayList = arrayListOf<String>()
+
+        Log.d("mytag", "Button clicked")
+
+        // send request
+        val queue = Volley.newRequestQueue(this)
+        val request = MyRequestArray(
+            this,
+            Request.Method.GET,
+            "/available-doctors",
+            null,
+            { response ->
+
+                Log.d("mytag", "$response")
+
+                for (i in 0 until response.length()) {
+
+                    val doctor: JSONObject = response.getJSONObject(i)
+
+
+                    // get the current student (json object) data
+                    var fname = doctor.getString("doc_fname")
+                    var lname = doctor.getString("doc_lname")
+
+                    var email = doctor.getString("doc_email")
+
+                    EmailSeslected.add(email.toString())
+
+                    arrayList.add("$fname $lname")
+                }
+                Log.d("mytag", "$arrayList , $EmailSeslected")
+
+            },
+            { error ->
+                Log.e("mytag", "Error: $error - Status Code = ${error.networkResponse?.statusCode}")
+                Toast.makeText(this, "Connection error", Toast.LENGTH_SHORT).show()
+            }
+        )
+        queue.add(request)
+        return arrayList
+    }
+
+    private fun sendMessage(res: String, message: String) {
+
+        val params = JSONObject()
+        params.put("doctor_email", res)
+        params.put("msg", message)
+
+        Log.d("mytag", "Button clicked")
+
+        // send request
+        val queue = Volley.newRequestQueue(this)
+        val request = MyRequest(
+            this,
+            Request.Method.POST,
+            "/contact-with-doctor",
+            params,
+            { response ->
+
+                Log.d("mytag", "response = $response")
+
+                // goto profile activity
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+                finish()
+
+                // if there is an error (wrong email or password)
+                if (response.has("error")) {
+                    val errorMesssage = response.getString("error")
+                    Toast.makeText(this, errorMesssage, Toast.LENGTH_SHORT).show()
+
+                }
+            },
+            { error ->
+                Log.e(
+                    "mytag",
+                    "Error: $error - Status Code = ${error.networkResponse?.statusCode}"
+                )
+                Toast.makeText(this, "Connection error", Toast.LENGTH_SHORT).show()
+            }
+        )
+        queue.add(request)
+
+    }
+
 }
